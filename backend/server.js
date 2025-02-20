c50cef09cddc504c333c6f514a4b072b60c4efe5
ErickAC12 <112605408+ErickAC12@users.noreply.github.com>
2025-02-20 11:32:00 -0600

    api productos

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,33 @@
 import express from 'express';
 const app = express();
 
-app.listen(5000, () => {
-  console.log("Server started at http://localhost:5000");
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { MongoClient } from 'mongodb';
+
+// Conexión con MongoDB
+async function connectToMongoDB() {
+  const client = new MongoClient(process.env.MONGO_URI);
+  await client.connect();
+  console.log('Conectado a MongoDB');
+  return client.db('tienda');
+}
+
+// Todos los productos
+app.get('/api/productos', async (req, res) => {
+  try {
+    const db = await connectToMongoDB();
+    const productosCollection = db.collection('productos');
+    const productos = await productosCollection.find().toArray();
+    res.json(productos);
+  } catch (error) {
+    console.error(error);
+  }
 });
+
+app.get('/api/')
+
+app.listen(5000, () => {
+  console.log('Server iniciado en http://localhost:5000');
+})
